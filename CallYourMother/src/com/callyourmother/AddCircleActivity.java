b4af79b62616ed513a20adf33f9b00eeedbc0359
tdastyar <tdastyar@gmail.com>
2013-11-30 21:06:47 -0500

    UI: Creating a contactadapter and continued changes to adding a circle
    NOTE: adding a contact still does not work

--- CallYourMother/src/com/callyourmother/AddCircleActivity.java
+++ CallYourMother/src/com/callyourmother/AddCircleActivity.java
@@ -1,20 +1,39 @@
 package com.callyourmother;
 
+import java.util.Iterator;
 import java.util.List;
+import java.util.Random;
+import java.util.Set;
 
 
 import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import com.callyourmother.data.Contact;
+import com.callyourmother.data.Contact.ContactNotFoundException;
+
+import android.provider.ContactsContract;
+import android.provider.ContactsContract.CommonDataKinds.Phone;
+import android.provider.ContactsContract.Contacts;
+import android.provider.ContactsContract.Data;
+import android.provider.ContactsContract.PhoneLookup;
+import android.util.Log;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 
 public class AddCircleActivity extends Activity {
 	ListView listView2;
-	CircleAdapter mAdapter;
+	ContactAdapter mAdapter; //placeholder
+	private static final int CONTACT_PICKER_RESULT = 1001;  
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -35,8 +54,51 @@ public class AddCircleActivity extends Activity {
 	
 		spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 		spinner.setAdapter(spinnerAdapter);
+		
+		TextView addContact = (TextView)findViewById(R.id.plus_contact);
+		
+		addContact.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				 Intent contactPickerIntent = new Intent(Intent.ACTION_PICK, Phone.CONTENT_URI);  
+	                startActivityForResult(contactPickerIntent, CONTACT_PICKER_RESULT);  
+			}
+		});
 	
 	}
+	
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {  
+	    if (resultCode == RESULT_OK) {  
+	   
+	    	Uri contactData = data.getData();
+	    	Cursor cur = managedQuery(contactData, null, null, null, null);
+	    	
+	 
+
+	    	if (cur.moveToFirst()) {
+	 	    	    String id = cur.getString(cur.getColumnIndex(ContactsContract.Contacts._ID));
+	 	    	    String name = cur.getString(cur.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+	    		
+		    	Log.i("DEBUG", "AddCircleActivity: Contact selected: " + name +" with ID: " + id); 
+	        
+	        
+		        try {
+					Contact newContact = new Contact(Long.parseLong(id), getApplicationContext());
+					mAdapter.add(newContact);
+				} catch (NumberFormatException e) {
+					Log.i("DEBUG", "AddCircleActivity NumberFormatException " + e);
+				} catch (ContactNotFoundException e) {
+					Log.i("DEBUG", "AddCircleActivity ContactNotFoundException " + e);						// BEING THROWN!!!??
+				}
+	        }
+	    	
+	    
+	    } else {  
+	        Log.i("DEBUG", "AddCircleActivity: activity result not ok. Contact id: " + CONTACT_PICKER_RESULT); 
+	      
+	    }  
+	}  
 	
 
 }
---
+++ CallYourMother/src/com/callyourmother/ContactAdapter.java
@@ -0,0 +1,77 @@
+package com.callyourmother;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.callyourmother.data.Circle;
+import com.callyourmother.data.Contact;
+import com.callyourmother.data.DatabaseClient;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class ContactAdapter extends ArrayAdapter<Contact> {
+
+	private final List<Contact> mItems = new ArrayList<Contact>();
+	private final Context mContext;
+	int layoutResourceId;
+	private DatabaseClient db;
+
+	
+    public ContactAdapter(Context context, int layoutResourceId) {
+        super(context, layoutResourceId);
+        this.layoutResourceId = layoutResourceId;
+        mContext = context;
+        //this.db = new DatabaseClient(context);
+        
+    }
+
+	@Override
+	public int getCount() {
+		return mItems.size();
+	}
+
+	@Override
+	public Contact getItem(int pos) {
+		return mItems.get(pos);
+	}
+
+	@Override
+	public long getItemId(int pos) {
+		return pos;
+	}
+
+	@Override
+	public void add(Contact contact) {
+		mItems.add(contact);
+		notifyDataSetChanged();
+	}
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		LinearLayout itemLayout;
+		//final Circle circle = mItems.get(position);
+
+		itemLayout = (LinearLayout) LayoutInflater.from(mContext).inflate(
+				R.layout.contact_item, parent, false);
+	
+
+		TextView nameView = (TextView) itemLayout.findViewById(R.id.contact_name_view);
+		nameView.setText(getItem(position).getDisplayName());
+		
+		TextView numberView = (TextView) itemLayout.findViewById(R.id.contact_number_view);
+		numberView.setText(getItem(position).getPhones().get(0).toString()); //just display first number
+		
+		ImageView photoView = (ImageView) itemLayout.findViewById(R.id.contact_image_view);
+		photoView.setImageBitmap(getItem(position).getPhoto());
+		
+		
+		return itemLayout;
+	}
+}
